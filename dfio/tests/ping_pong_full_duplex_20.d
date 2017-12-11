662d0703c4600eabc17ddd24eff2b10f27efad87
Alexandru Caciulescu <alex.darredevil@gmail.com>
2017-12-11 20:49:31 +0200

    WIP test duplex 20

--- dfio/tests/ping_pong_full_duplex_20.d
+++ dfio/tests/ping_pong_full_duplex_20.d
@@ -3,10 +3,13 @@ import core.sys.posix.unistd : write, _exit;
 import core.sys.posix.sys.types;
 import std.socket;
 import core.stdc.errno;
+import core.stdc.string;
+import std.string;
 import core.sys.posix.sys.socket;
 import core.sys.posix.fcntl;
 import core.thread;
 import core.sys.posix.stdlib: abort;
+import std.conv : to;
 import dfio;
 
 void check(int code) {
@@ -15,15 +18,15 @@ void check(int code) {
 }
 
 // if this writes say 100 bytes total
-void writerReader(int fd1, int fd2) {
+void writerReader(int fd1, int fd2, string toSend, string toRecv) {
     logf("<started writerReader, fd1 = %d, fd2 = %d>", fd1, fd2);
     auto s = "simple read write\n";
-    write(fd1, s.ptr, s.length).checked;
+    write(fd1, toSend.ptr, toSend.length).checked;
 
     logf("<midway writerReader, fd1 = %d, fd2 = %d>", fd1, fd2);
 
-    char[100] buf2;
-    ssize_t total = 17;
+    char[100] buf2 = 0;
+    ssize_t total = toRecv.length;
     ssize_t bytes = 0;
     while(bytes < total) {
         ssize_t resp = read(fd2, buf2.ptr + bytes, total - bytes).checked;
@@ -31,14 +34,16 @@ void writerReader(int fd1, int fd2) {
         bytes += resp;
     }
 
-    logf("<finished writerReader>");
+    assert(cmp(fromStringz(buf2.ptr), toRecv) == 0);
+
+    logf("<finished writerReader, fd1 = %d, fd2 = %d>", fd1, fd2);
 }
 
 // it must read the exact same amount (in total) that would be 100 bytes
-void readerWriter(int fd1, int fd2) {
+void readerWriter(int fd1, int fd2, string toSend, string toRecv) {
     logf("<started readerWriter, fd1 = %d, fd2 = %d>", fd1, fd2);
-    char[100] buf;
-    ssize_t total = 17;
+    char[100] buf = 0;
+    ssize_t total = toRecv.length;
     ssize_t bytes = 0;
     while(bytes < total) {
         ssize_t resp = read(fd1, buf.ptr + bytes, total - bytes).checked;
@@ -46,50 +51,59 @@ void readerWriter(int fd1, int fd2) {
         bytes += resp;
     }
 
+    assert(cmp(fromStringz(buf.ptr), toRecv) == 0);
     logf("<midway readerWriter, fd1 = %d, fd2 = %d>", fd1, fd2);
 
     auto s = "simple read write\n";
     char[] buf2 = s.dup;
-    write(fd2, s.ptr, s.length).checked;
-    logf("<finished readerWriter>");
+    write(fd2, toSend.ptr, toSend.length).checked;
+    logf("<finished readerWriter, fd1 = %d, fd2 = %d>", fd1, fd2);
 }
 
 void main() {
-    // C: int[20][4] x;......  x[19][3]... x[3][19]
-    int[2][20] socks;
+    enum int NR = 8;
+    int[2][NR] socks;
+    string s1 = "first read write\n";
+    string s2 = "second read write\n";
     startloop();
-    for(int i = 0; i < 2; i++) {
-        //pragma(msg, typeof(socks[i]));
+    for(int i = 0; i < NR; i++) {
         check(socketpair(AF_UNIX, SOCK_STREAM, 0, socks[i]));
-        logf("socks[%d] = %s, length = %d", i, socks[i], socks[i].length);
-
+        //logf("socks[i] = %s", i, socks[i]);
     }
-    writefln("socks = %s", socks);
+
+    logf("socks = %s", socks);
+
+
     // spawn a thread to run I/O loop
     // spawn thread to write stuff
     Thread[] wrs;
-    for(int i = 0; i < 2; i+=2) {
-        //writefln("i = %d, i+1 = %d", i, i+1);
-        //writefln("socks[i] = %s, socks[%d][1] = %d", socks[i], i, socks[i][1]);
-        auto wr = new Thread(() => writerReader(socks[i][0], socks[i+1][0]));
+    for(int i = 0; i < NR; i += 2) {
+        auto a1 = socks[i][0];
+        auto a2 = socks[i+1][0];
+        //auto wr = new Thread(() => writerReader(socks[i][0], socks[i+1][0], s2, s1));
+        auto wr = new Thread(() => writerReader(a1, a2, s2, s1)); // BUG: must use temp vars a1,a2 else it crashes
+        logf("wr = %s", cast(void*)wr);
         wr.start();
         wrs ~= wr;
     }
 
     // spawn fiber to read stuff
-    for(int i = 0; i < 2; i+=2) {
-        pragma(msg, typeof(socks));
-        pragma(msg, typeof(socks[i]));
-        pragma(msg, typeof(socks[0][i]));
-        writefln("i = %d, i+1 = %d", i, i+1);
-        writefln("socks[i] = %s, socks[%d][1] = %d", socks[i], i, socks[i][1]);
-        spawn(() => readerWriter(
-            socks[i][1],
-            socks[i+1][1]));
+    for(int i = 0; i < NR; i += 2) {
+        if ((i+1) <= socks.length && socks[i].length > 1) {
+            stderr.writefln("socks[i][1] = %d, socks[i][1+1] = %d", socks[i][1], socks[i+1][1]);
+            auto a1 = socks[i][1];
+            auto a2 = socks[i+1][1];
+            spawn(() => readerWriter( // BUG: must use temp vars a1,a2 else it crashes
+                //socks[i][1],
+                a1,
+                //socks[qi+1][1],
+                a2,
+                s1, s2));
+        }
     }
     runUntilCompletion();
     //
-    for(int i = 0; i < 10; i++) {
-        wrs[i].join();
+    foreach(w; wrs) {
+        w.join();
     }
 }
