441fd486fa978ee7cf98eb39b4ce7270ce5c234e
Dmitry Olshansky <dmitry@olshansky.me>
2018-01-31 14:33:19 +0300

    Add Java Undertow application server as benchmark example
    It is not trivial to monitor memory usage with our abc script
    and build instructions are not there yet but I wanted to have
    Java sample as well. It actually performs very well.

---
+++ dfio/bench/static_http/hello_undertow/src/main/java/me/olshansky/HelloUndertow.java
@@ -0,0 +1,21 @@
+package me.olshansky;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+public class HelloUndertow {
+    public static void main(final String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler(new HttpHandler() {
+
+                    public void handleRequest(final HttpServerExchange exchange) {
+                        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                        exchange.getResponseSender().send("Hello, world!");
+                    }
+                }).build();
+        server.start();
+    }
+}
